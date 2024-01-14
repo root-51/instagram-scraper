3088b41bf19cbf7afd10c5d2e47794ca1bacc7c3
JangSoyun <soyoon5328@gmail.com>
2024-01-14 10:08:26 +0900

    feat: update num of posts in Enterprise table

--- src/database_manager.py
+++ src/database_manager.py
@@ -10,11 +10,21 @@ def csvToDataframe(fileName):
         print(e)
         return None
 
-def createDatebase(databaseName):
-    return sql.connect('databases/'+databaseName+'.db')
 
-def DataframeToDatabase(dataframe, db_name, table_name):
-    conn = sql.connect('databases/'+db_name+'.db')
-    dataframe.to_sql(table_name, conn, if_exists='replace', index=False)
+def createDatebase(databaseName):
+    return sql.connect("databases/" + databaseName + ".db")
+
+
+def dataframeToDatabase(dataframe, db_name, table_name):
+    conn = sql.connect("databases/" + db_name + ".db")
+    dataframe.to_sql(table_name, conn, if_exists="replace", index=False)
+    conn.commit()
+    conn.close()
+
+def updateNumOfPosts(enterprise_name, value):
+    conn = sql.connect("databases/test01.db")
+    cursor = conn.cursor()
+    query = f"UPDATE Enterprise SET num_of_posts = {value} WHERE enterprise_name = '{enterprise_name}'; "
+    cursor.execute(query)
     conn.commit()
     conn.close()
