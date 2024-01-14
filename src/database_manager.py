31ded8dc29763f3b249e427a02316883d5efbf30
JangSoyun <soyoon5328@gmail.com>
2024-01-14 10:55:13 +0900

    refact: extract method(connectDatabase, createCursor, finishQuery)

--- src/database_manager.py
+++ src/database_manager.py
@@ -16,15 +16,27 @@ def createDatebase(databaseName):
 
 
 def dataframeToDatabase(dataframe, db_name, table_name):
-    conn = sql.connect("databases/" + db_name + ".db")
+    conn = connectDatabase(db_name)
     dataframe.to_sql(table_name, conn, if_exists="replace", index=False)
-    conn.commit()
-    conn.close()
+    finishQuery(conn)
+
 
 def updateNumOfPosts(enterprise_name, value):
-    conn = sql.connect("databases/test01.db")
-    cursor = conn.cursor()
+    conn = connectDatabase("test01")
+    cursor = createCursor(conn)
     query = f"UPDATE Enterprise SET num_of_posts = {value} WHERE enterprise_name = '{enterprise_name}'; "
     cursor.execute(query)
+    finishQuery(conn)
+
+
+def connectDatabase(database_name: str) -> sql.Connection:
+    return sql.connect(f"databases/{database_name}.db")
+
+
+def createCursor(conn: sql.Connection) -> sql.Cursor:
+    return conn.cursor
+
+
+def finishQuery(conn: sql.Connection) -> None:
     conn.commit()
     conn.close()
